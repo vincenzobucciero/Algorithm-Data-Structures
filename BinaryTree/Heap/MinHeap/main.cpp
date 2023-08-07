98aedb8f8a49298e0f0c3c1724e0f85fb1291347
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2023-08-07 12:18:35 +0200

    *ag

---
+++ BinaryTree/Heap/MinHeap/main.cpp
@@ -0,0 +1,30 @@
+/**
+ * Progettare ed implementare una classe template Min-Heap dotata delle seguenti operazioni
+    • Min-Heapify
+    • Build-Min-Heap
+    • Insert
+    • PrintArray
+    • (PrintAsciiTree)
+*/
+
+#include <iostream>
+#include <vector>
+
+#include "minHeap.h"
+
+using namespace std;
+
+int main() {
+    MinHeap<int> myTree;
+
+    myTree.insert(1);
+    myTree.insert(2);
+    myTree.insert(3);
+    myTree.insert(4);
+    myTree.insert(5);
+    myTree.insert(6);
+    myTree.insert(7);
+
+    myTree.printTree();
+    myTree.printAsciiTree();
+}
