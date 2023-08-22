1cc41865e46e152a4ac4c04c3014f000902cdf15
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2023-08-22 11:21:59 +0200

    //TODO

---
+++ programmazioneGreedy/Esercizio01/main.cpp
@@ -0,0 +1,10 @@
+/**
+ * Progettare ed implementare un codificatore/decodificatore di Huffman
+    • Estrarre la frequenza di ogni lettera
+        – file contenente le coppie <lettera,frequenza>
+        – oppure file contenente solo lettere
+        – oppure file contenente parole
+        – negli ultimi due casi si può utilizzare l'esercizio wordcount visto nella lezione 5
+    • Per la costruzione/gestione del min-heap/min-priority- queue si possono utilizzare gli esercizi 
+        delle lezioni 9 e 10
+*/
