1864e8b8079e7571b0e1fd56672742e0d4ee5878
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2023-09-15 14:04:46 +0200

    ye

---
+++ PreparazioneEsame/Grafi/bfs/bfs5/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+#include "vertice.h"
+#include "nodo.h"
+#include "grafo.h"
+
+using namespace std;
+
+int main() {
+    Grafo<int> grafo;
+
+    ifstream file;
+    string myFileInput = "fileInput.txt";
+    file.open(myFileInput);
+
+    string line;
+
+    int i = 0, numVertici = 0, numArchi = 0, vertice, arco, verticeSorgente;
+
+    while(getline(file, line)) {
+        stringstream lineIn(line);
+        if(i == 0) {
+            lineIn >> numVertici;
+            lineIn >> numArchi;
+            i++;
+        } else {
+            lineIn >> vertice;
+            lineIn >> arco;
+
+            Vertice<int>* v1 = new Vertice<int>(vertice);
+            Vertice<int>* v2 = new Vertice<int>(arco);
+
+            verticeSorgente = vertice;
+
+            if(grafo.searchIndirizzo(v1) == -1)
+                grafo.addNodo(v1);
+            if(grafo.searchIndirizzo(v2) == -1)
+                grafo.addNodo(v2);
+            grafo.addArco(grafo.getIndirizzoVertice(vertice), grafo.getIndirizzoVertice(arco));
+        }
+    }
+
+    file.close();
+
+    cout << "Grafo PRE-BFS" << endl;
+    cout << grafo << endl;
+    cout << "Grafo POST-BFS" << endl;
+    grafo.bfs(grafo.getIndirizzoVertice(verticeSorgente));
+    cout << grafo << endl;
+
+    return 0;
+}
---
+++ PreparazioneEsame/Grafi/bfs/bfs5/vertice.h
@@ -0,0 +1,60 @@
+#ifndef VERTICE_H
+#define VERTICE_H
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <queue>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+enum class Color {
+    white, gray, black
+};
+
+template <typename T>
+class Vertice {
+    private:
+        T value;
+        Color color;
+        Vertice<T>* predecessore;
+        int distanza;
+    public:
+        Vertice(T value);
+
+        void setValue(T value) {this->value = value;}
+        void setColor(Color color) {this->color = color;}
+        void setPredecessore(Vertice<T>* predecessore) {this->predecessore = predecessore;}
+        void setDistanza(int distanza) {this->distanza = distanza;}
+
+        T getValue() {return this->value;}
+        Color getColor() {return this->color;}
+        Vertice<T>* getPredecessore() {return this->predecessore;}
+        int getDistanza() {return this->distanza;}
+
+        friend ostream& operator<<(ostream& out, const Vertice<T>& obj) {
+            string c;
+            switch(obj.color) {
+                case Color::white: c = "white"; break;
+                case Color::gray: c = "gray"; break;
+                case Color::black: c = "black"; break;
+                default: break;
+            }
+            out << "value -> " << obj.value << ", color -> " << c << endl;
+            return out;
+        }
+};
+
+template <typename T>
+Vertice<T>::Vertice(T value) {
+    this->value = value;
+    this->color = Color::white;
+    this->predecessore = nullptr;
+    this->distanza = UINT16_MAX;
+}
+
+
+#endif
